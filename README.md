c7ae118fc55b4b5fb6e8f43f64c8316ef5e298f5
hayaten0415 <hayaten415@gmail.com>
2022-05-12 15:59:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# MicroBlog
+Next.js Blog App
+
+# Included
+- TypeScript
+- Next.js
+- Tailwind CSS
---
+++ pages/posts/firstPost.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const firstPost:NextPage = () => {
+  return (
+    <div>
+      <h1>最初の投稿</h1>
+      <h2>ホームへ戻る</h2>
+    </div>
+  );
+};
+
+export default firstPost;
